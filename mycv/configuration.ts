f51bfaedf80c7a55e9399114813b24a3c2098d45
MaxChou1205 <v2asault@yahoo.com.tw>
2024-08-13 23:51:24 +0800

    add db connection config

---
+++ mycv/configuration.ts
@@ -0,0 +1,5 @@
+export interface EnvironmentVariables {
+  DATABASE_HOST: string;
+  DATABASE_USER: string;
+  DATABASE_PASSWORD: string;
+}
--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   providers: [AppService],
   controllers: [AppController],
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: new ConfigService().get('DATABASE_HOST'),
+      port: parseInt(new ConfigService().get('DATABASE_PORT')),
+      username: new ConfigService().get('DATABASE_USER'),
+      password: new ConfigService().get('DATABASE_PASSWORD'),
+      database: new ConfigService().get('DATABASE_NAME'),
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
 })
 export class AppModule {}
